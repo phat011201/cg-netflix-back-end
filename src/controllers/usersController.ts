6b533bd446aaac8ce5baa867dfa8e5b0edbdb2a1
Phat Huynh <phatht011201@gmail.com>
2023-12-21 10:02:54 +0700

    Add bcrypt for password hashing and update userController methods

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -2,6 +2,7 @@
 
 // import modules
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
 
 // import users model
 import Users from "../models/usersModels";
@@ -14,12 +15,14 @@ const usersController = {
   getAllUsers: (req: Request, res: Response) => {
     res.status(200).json(usersData);
   },
+
   getUserById: (req: Request, res: Response) => {
     const id = Number(req.params.id);
     const user = usersData.find((user: any) => user.id === id);
     res.status(200).json(user);
   },
-  addUser: (req: Request, res: Response) => {
+
+  addUser: async (req: Request, res: Response) => {
     const { id, email, password } = new Users(
       req.body.id,
       req.body.name,
@@ -27,27 +30,48 @@ const usersController = {
       req.body.email,
       req.body.password
     );
-    const newUser: any = {
-      id,
-      email,
-      password,
-    };
-    usersData.push(newUser);
-    res.status(201).json(newUser);
+
+    try {
+      const hashedPassword = await hashPassword(password);
+
+      const newUser: any = {
+        id,
+        email,
+        password: hashedPassword,
+      };
+
+      usersData.push(newUser);
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(500).json({ message: "Error adding user" });
+    }
   },
-  updateUser: (req: Request, res: Response) => {
+
+  updateUser: async (req: Request, res: Response) => {
     const id = Number(req.params.id);
     const user: any = usersData.find((user: any) => user.id === id);
     if (user) {
       user.name = req.body.name;
       user.nickname = req.body.nickname;
       user.email = req.body.email;
-      user.password = req.body.password;
-      res.status(200).json(user);
+
+      try {
+        const hashedPassword = await hashPassword(req.body.password);
+
+        user.password = hashedPassword;
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(500).json({ message: "Error updating user" });
+      }
     } else {
       res.status(404).json({ message: "User not found!" });
     }
   },
+};
+
+const hashPassword = async (password: string) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return hashedPassword;
 };
 
 export default usersController;
